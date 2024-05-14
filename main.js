92cbcb664894fa84b8c8b7521eb42a6dcb3b8758
yxrael <yxrael@gmail.com>
2024-05-14 08:14:09 +0200

    inicio config

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+console.log('working');
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
